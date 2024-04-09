f8dd10e36b1781c111aa1d98e8d2cf03665d7ea9
ShoxruxDevolopment <shoxruxrustamovshoxfitnes@gmail.com>
2024-04-09 09:35:28 +0500

    add main.js and add class dark mode

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const elbtn=document.querySelector('.btn');
+// const elbtnRemove=document.querySelector('.btn');
+ elbtn.addEventListener("click", function(){
+  document.body.classList.add('dark-mode');
+
+})
